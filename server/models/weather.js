e3323d18d9656e2249bdeb72c2c43effd8a6824d
jdraper9 <jamesdraper24@gmail.com>
2020-05-27 13:17:59 -0700

    added backend code

---
+++ server/models/weather.js
@@ -0,0 +1,21 @@
+const request = require('request-promise');
+
+const API_KEY = '33030d1a268b58ee76fabfa5f60caf97';
+
+class Weather {
+  static retrieveByCity(city, callback) {
+    request({
+      uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+      json: true,
+    })
+      .then(function (res) {
+        callback(res);
+      })
+      .catch(function (err) {
+        console.log(err);
+        callback({ error: 'Could not reach OpenWeatherMap API.' });
+      });
+  }
+}
+
+module.exports = Weather;
